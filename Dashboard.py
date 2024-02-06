b173939abe0ff0f5ba7f6993b3d15a4464e875ae
HenamSingla <singlahenam45@gmail.com>
2024-02-06 17:32:46 -0600

    Working on it!

--- Dashboard.py
+++ Dashboard.py
@@ -1,7 +1,19 @@
-import requests
-from bs4 import BeautifulSoup
-
+import pandas as pd
 import nfl_data_py as nfl
 
-# Fetch data for a specific player
-pbp = nfl.import_pbp_data([1990: 2022])
+# Initialize an empty DataFrame to store all the pbp data
+all_years_pbp_data = pd.DataFrame()
+
+for year in range(1999, 2024): 
+    # Correctly pass the year as a list to comply with the function's requirements
+    year_pbp_data = nfl.import_pbp_data([year])
+    
+    # Append the data for the current year to the cumulative DataFrame
+    all_years_pbp_data = pd.concat([all_years_pbp_data, year_pbp_data], ignore_index=True)
+
+
+# Display the first 5 rows
+print(all_years_pbp_data.head())
+# Get descriptive statistics for numeric columns
+print(all_years_pbp_data.shape)
+
