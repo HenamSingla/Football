cf730325f23b04559624f627ce10187d9effd35f
HenamSingla <singlahenam45@gmail.com>
2024-02-05 21:41:01 -0600

    Working on it

--- Dashboard.py
+++ Dashboard.py
@@ -2,5 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 
 import nfl_data_py as nfl
-nfl.import_pbp_data(years, columns, downcast=True, cache=False, alt_path=None)
 
+# Fetch data for a specific player
+pbp = nfl.import_pbp_data([1990: 2022])
